b7db4c3e0f388229e219ff8db9b8eb9a97728359
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-04 11:40:04 +0300

    Create Program.cs

---
+++ Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+ArrayList array = new ArrayList();
+ArrayList enKucuk = new ArrayList();
+ArrayList enBuyuk = new ArrayList();
+
+int toplam = 0, toplam2 = 0;
+
+
+for(int i = 0; i < 20; i++){
+    Console.WriteLine("Sayi giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    array.Add(sayi);
+}
+foreach (var item in array)
+{
+    Console.Write(item+" ");
+}
+array.Sort();
+Console.Write("\n");
+Console.WriteLine("***siralanmis***");
+
+foreach (var item in array)
+{
+    Console.Write(item+" ");
+}
+
+for(int i = 0; i < 3; i++){
+    enKucuk.Add(array[i]);
+}
+
+for(int i = array.Count-1; i > array.Count-4; i--){
+    
+    enBuyuk.Add(array[i]);
+    
+}
+Console.Write("\n");
+Console.WriteLine("*****en kucuk*****");
+foreach (var item in enKucuk)
+{
+    Console.Write(item+" ");
+    toplam = toplam + (int)item;
+}
+int ortalama = toplam/3;
+Console.Write("\n");
+Console.Write("Ortalama: "+ortalama);
+Console.Write("\n");
+Console.WriteLine("*****en buyuk*****");
+foreach (var item in enBuyuk)
+{
+    Console.Write(item+" ");
+    toplam2 = toplam2 + (int)item;
+}
+int ortalama2 = toplam2/3;
+int ortalamaToplamlari = ortalama+ortalama2;
+Console.Write("\n");
+Console.Write("Ortalama: "+ortalama2);
+Console.Write("\n");
+Console.Write("Ortalama toplamlari: "+ortalamaToplamlari);
+
+
+
+
