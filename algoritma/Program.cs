f865d5ee5ae5c8653476af2684388e21d24719c3
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-08 20:31:05 +0300

    Create Program.cs

---
+++ algoritma/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Bir kelime ve sayi gir. Kelime ve sayi arasinda virgul olsun: ");
+string deger = Console.ReadLine();
+
+string[] dizi = deger.Split(",");
+
+int cevir = Convert.ToInt32(dizi[1]);
+string yeni = dizi[0];
+for(int i = 0; i < dizi[0].Length; i++){
+
+    if(i == cevir){
+        yeni = yeni.Remove(cevir,1);
+    }
+    
+} 
+
+foreach (var item in yeni)
+{
+    Console.Write(item);
+}
