46a207e670009a01523c9c610a80d369cf76b852
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-08 19:45:23 +0300

    Create Program.cs

---
+++ ucgenCizme/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ucgen boyutunu giriniz: ");
+int boyut = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= boyut; i++){
+    for(int j = 0; j < i; j++){
+        Console.Write("*");
+    }
+    Console.Write("\n");
+}
