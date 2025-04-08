38c7b78c11aae43ce0a1c9023c4af604ddb9c890
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-08 19:20:35 +0300

    Create Program.cs

---
+++ ortalamaHesaplama/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Fibonacci derinligini giriniz: ");
+int derinlik = Convert.ToInt32(Console.ReadLine());
+
+int[] dizi = new int[derinlik];
+dizi[0] = 1;
+dizi[1] = 1;
+int toplam = 2;
+
+for(int i = 2; i < derinlik; i++){
+    toplam = dizi[i-1] + dizi[i-2];
+    dizi[i] = toplam;
+}
+
+foreach (int item in dizi)
+{
+    Console.Write(item+" ");
+}
