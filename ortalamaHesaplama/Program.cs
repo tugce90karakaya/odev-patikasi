a1762c17a1c571f23e8690a79af51088f3c4d9b2
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-08 19:27:39 +0300

    Update Program.cs

--- ortalamaHesaplama/Program.cs
+++ ortalamaHesaplama/Program.cs
@@ -6,12 +6,22 @@ dizi[0] = 1;
 dizi[1] = 1;
 int toplam = 2;
 
+
 for(int i = 2; i < derinlik; i++){
     toplam = dizi[i-1] + dizi[i-2];
     dizi[i] = toplam;
 }
 
+float toplam2 = 0;
+
 foreach (int item in dizi)
 {
+    toplam2 = toplam2 + item;
     Console.Write(item+" ");
+    
 }
+float ortalama = toplam2 / dizi.Length;
+
+Console.WriteLine("Ortalama: "+ortalama);
+
+
