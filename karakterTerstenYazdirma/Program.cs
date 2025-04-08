da861047a5be59f45a7500306952c3094b1b98d5
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-08 21:01:59 +0300

    Create Program.cs

---
+++ karakterTerstenYazdirma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Metin giriniz: ");
+        string input = Console.ReadLine();
+
+        string[] kelimeler = input.Split(' ');
+        for (int i = 0; i < kelimeler.Length; i++)
+        {
+            string kelime = kelimeler[i];
+            if (kelime.Length > 1)
+            {
+                // Son harfi al, başa koy. Geri kalanı sona ekle
+                string yeniKelime = kelime.Substring(1) + kelime[0];
+                kelimeler[i] = yeniKelime;
+            }
+        }
+
+        // Yeni halini birleştir ve yazdır
+        string sonuc = string.Join(" ", kelimeler);
+        Console.WriteLine("Çıktı: " + sonuc);
+    }
+}
