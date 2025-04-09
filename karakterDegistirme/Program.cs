bccc7a2ba838605e1430b096446ef2a3f601c6e9
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-09 15:42:59 +0300

    Create Program.cs

---
+++ karakterDegistirme/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Bir metin giriniz: ");
+string metin = Console.ReadLine();
+
+string[] dizi = metin.Split(" ");
+char a;
+char b;
+
+// merhaba
+// aerhabm
+
+
+foreach (string item in dizi)
+{
+    a = item[0];
+    b = item[item.Length-1];
+    string deger = b+item.Substring(1,item.Length-2)+a;
+    Console.Write(deger+" ");
+}
