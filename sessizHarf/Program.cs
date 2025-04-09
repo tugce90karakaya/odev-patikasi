bb23d6ee00f6cde8e63d929d0061d5b115c4b13c
Tugce Karakaya <tugcekarakaya90@gmail.com>
2025-04-09 17:37:19 +0300

    Create Program.cs

---
+++ sessizHarf/Program.cs
@@ -0,0 +1,24 @@
+﻿
+        Console.WriteLine("Bir metin gir: ");
+        string metin = Console.ReadLine();
+        string[] kelimeler = metin.Split(" ");
+
+        foreach (string kelime in kelimeler)
+        {
+            bool ikiSessizYanYanaVar = false;
+            string kucukKelime = kelime.ToLower();
+            string sesliler = "aeiou";
+
+            for (int i = 0; i < kucukKelime.Length - 1; i++)
+            {
+                if (!sesliler.Contains(kucukKelime[i]) && !sesliler.Contains(kucukKelime[i + 1]))
+                {
+                    ikiSessizYanYanaVar = true;
+                    break;
+                }
+            }
+
+            Console.Write(ikiSessizYanYanaVar + " ");
+        }
+    
+
